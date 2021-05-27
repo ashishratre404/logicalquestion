8289d32a4528569d78c2784eab53547dedbbb7bd
ASHISH RATRE <66317438+45HI5H-R47R3@users.noreply.github.com>
2021-05-27 19:41:00 +0530

    Updates

---
+++ precache-manifest.660fe7a2a5b662bd9d8ba2054c739cfb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c311f57d9146a474d00",
+    "url": "/logicalquestion/static/js/main.8957a9cb.chunk.js"
+  },
+  {
+    "revision": "a7ca703fe5c0b71b569e",
+    "url": "/logicalquestion/static/js/runtime~main.43dd077f.js"
+  },
+  {
+    "revision": "ac0ead07279d7ef70242",
+    "url": "/logicalquestion/static/js/2.f1ffa9bd.chunk.js"
+  },
+  {
+    "revision": "e475cd97ef39caf140da12d3afc53045",
+    "url": "/logicalquestion/index.html"
+  }
+];
